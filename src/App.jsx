d4fff68185d8978c03d7be83af6e259380dfaad4
joaokiller360 <joao1.joao3@gmail.com>
2023-12-29 23:20:48 -0500

    Adaptacion del nav y practica de Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './stylesheets/App.css';
 import { Menu } from './componentes/Menu/menu.jsx';
 import { CampoTestimonio } from './componentes/testimonio/campotestimonio';
-import { BrowserRouter, Routes, Route } from  'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from  'react-router-dom';
 import { Error404 } from './componentes/error404/error404.jsx';
 import { Inicio } from './componentes/home/inicio.jsx' ;
 
@@ -14,6 +14,7 @@ function App() {
           <Routes>
               <Route path='/' element={<Inicio /> }/>
               <Route path='/testimonio' element={<CampoTestimonio />} />
+              <Route path='/cards' element={<Navigate to='/' />} />
               <Route path='*' element={<Error404 />} />
           </Routes>
         </div>
--- src/componentes/Menu/menu.jsx
+++ src/componentes/Menu/menu.jsx
@@ -7,16 +7,16 @@ export function Menu () {
     <nav id="menu">
     <a class="item"><NavLink id='navlink' to='/'>Inicio</NavLink></a>
     <a class="item"><NavLink id='navlink' to='/testimonio'>Testimonio</NavLink></a>
+    <a class="item"><NavLink id='navlink' to='/cards'>Cards</NavLink></a>
     <div class="item">
       Paginas Extras
-      <div class="dropdown">
+      <div class="dropdown">  
         <div>
           <a><NavLink id='navlink' to='*'>Error 404</NavLink></a>
         </div>
       </div>
     </div>
     <a class="item">Contact</a>
-    <div class="underline"></div>
   </nav>
   )
 };
